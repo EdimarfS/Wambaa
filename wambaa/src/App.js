fead9d6a71b871ed1c9b7cec9c1008760a66be58
edimarf <edmarfyelloww@gmail.com>
2021-02-10 06:39:34 +0100

    Wambaa --> Components, Navigation, and Containers done --> React DOM installed

--- wambaa/src/App.js
+++ wambaa/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Wambaa from './Containers/Wambaa';
 
 function App() {
   return (
     <div className="App">
+      <Wambaa/>
+   
 
     </div>
   );
---
+++ wambaa/src/Containers/Wambaa.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Wambaa() {
+    return (
+        <div>
+            <p>Wambaa welcome page!</p>
+        </div>
+    )
+}
+
+export default Wambaa
